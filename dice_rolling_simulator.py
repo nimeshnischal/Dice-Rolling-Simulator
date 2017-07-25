7c162b30c8784ad11a634d3f21a0bf705257108a
Nimesh_Nischal <n.nischal.1994@gmail.com>
2017-07-25 17:11:33 +0530

    Introduced my custom module and refactored code
    Imported nimesh_app_basics module.
    Refactored to remove warnings and make the code more understandable and clean.

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,98 +1,57 @@
 #This is a basic python program to simulate the rolling of a dice
 
-import random
-import os   
+import nimesh_app_basics
 
-def clear_screen():                     #Function to clear the console screen
-    clear = lambda:os.system('cls')
-    clear()
 
-def print_welcome_message():
-    welcome_message = " Welcome to Dice Rolling Simulator ".center(100, "_")
-    print("\n", welcome_message, "\n")
-
-def get_user_choice():
-    user_choice = None
-    while user_choice not in user_choice_options:   #Run the loop unless a valid user's choice is received
-        user_choice = input()
-        user_choice = user_choice.casefold()
-        if user_choice not in user_choice_options:
-            print("Wrong input, please try again! (y/n):")
-            continue
-        return user_choice
-
-def ask_user_for_default_choice():
-    print("Do you want to use the default 6-faced dice? (y/n)")
-    return get_user_choice()
-
-def get_min_max_values():
-    if user_choice in user_choice_no_options:
+def get_min_max_values(user_choice_default_dice_boolean):
+    if not user_choice_default_dice_boolean:
         while True:
             print("Please enter the minimum and maximum values on the dice to simulate.")
-            min_value, max_value = get_int_min_max_input()
-            if min_value >= max_value:          			#Checking for valid entries
+            min_val = nimesh_app_basics.get_int_input("Minimum: ")
+            max_val = nimesh_app_basics.get_int_input("Maximum: ")
+            # Checking for valid entries
+            if min_val >= max_val:
                 print("Invalid entries, please try again!")
                 continue
             else:
                 break
     else:
-        min_value = 1
-        max_value = 6
-    return [min_value, max_value]
+        min_val = 1
+        max_val= 6
+    return [min_val, max_val]
 
-def get_int_min_max_input():
-    try:
-        min_value = int(input())
-        max_value = int(input())
-    except ValueError:
-        print("Error! Please enter integer values.")
-        return get_int_min_max_input()
-    else:
-        return min_value, max_value
-
-
-def get_random_number(min_num, max_num):        #Function to generate a random number between two numbers, including those two numbers
-    return random.randint(min_num,max_num)
-
-def print_exit_message():
-    exit_message = " Thank you for using this app. Developed by Nimesh Nischal. ".center(100,"=")
-    print("\n", exit_message, "\n")
-    input("Press enter to exit!")
 
 def show_rolled_dice_value_and_loop(min_value, max_value):
-    user_choice = 'y'
-    while user_choice not in user_choice_no_options:
+    play_again_boolean = True
+    while play_again_boolean:
         print("And the dice rolls....................")
-        print("\nThe number is: ", get_random_number(min_value, max_value))     #TODO Draw ascii art for numbers (or use number images in GUI)
-        print("\nRoll again? (y/n)")
-        user_choice = get_user_choice()
-        if user_choice in user_choice_no_options:
-          print_exit_message()
+        print("\nThe number is: ", nimesh_app_basics.get_random_int_in_range(min_value, max_value))     #TODO Draw ascii art for numbers (or use number images in GUI)
+        play_again_boolean = nimesh_app_basics.get_valid_y_n_opinion("\nRoll again? ( y / n ): ")
+        if not play_again_boolean:
+          nimesh_app_basics.print_exit_message(app_name)
           break
         else:
-          clear_screen()
-          print_welcome_message()
+          nimesh_app_basics.clear_screen()
+          nimesh_app_basics.print_welcome_message(app_name)
           continue
 
 
 #===================== MAIN PROGRAM =====================#
+app_name = "Dice Rolling Simulator"
 
-user_choice_options = ['y', 'n', 'yes', 'no']    #Taking most of the possible user inputs in cosideration
-user_choice_no_options = ['n','no']              #Until now, no need to define user_choice_yes_options list
+nimesh_app_basics.clear_screen()
+nimesh_app_basics.print_welcome_message(app_name)
 
-clear_screen()
-print_welcome_message()
+user_choice_default_dice_boolean = nimesh_app_basics.get_valid_y_n_opinion("Do you want to use the default 6-faced dice? ( y / n ): ")
 
-user_choice = ask_user_for_default_choice()
+nimesh_app_basics.clear_screen()
+nimesh_app_basics.print_welcome_message(app_name)
 
-clear_screen()
-print_welcome_message()
-
-min_max_values = get_min_max_values()
+min_max_values = get_min_max_values(user_choice_default_dice_boolean)
 min_value = min_max_values[0]
 max_value = min_max_values[1]
 
-clear_screen()
-print_welcome_message()
+nimesh_app_basics.clear_screen()
+nimesh_app_basics.print_welcome_message(app_name)
 
 show_rolled_dice_value_and_loop(min_value, max_value)
